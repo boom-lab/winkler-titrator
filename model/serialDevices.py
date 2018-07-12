47730d373d7b128dbd775dc32081bd6258b76c37
dnicholson <whoiboomlab@gmail.com>
2018-07-12 16:09:56 -0400

    added UBC serialDevices.py version

--- model/serialDevices.py
+++ model/serialDevices.py
@@ -6,6 +6,10 @@ Created on Wed Dec 20 15:09:00 2017
 @author: dnicholson
 """
 
+#CHANGES TRACKED WITH:
+###########################################################################
+# RI
+
 import serial
 import time
 
@@ -15,7 +19,8 @@ class meter(serial.Serial):
     Be sure meter probe and serial cables are connected
     """       
 
-    def readline(self,eol=b'\r'):
+    def readline(self,eol=b'\r'): ###########################################
+    #def readline(self,eol='\r'):
         """
         read line of output -  meter uses '\r' terminator. replaces 
         Serial.serial.readline() which only works with '\n'
@@ -41,7 +46,7 @@ class meter(serial.Serial):
         """
         if self.in_waiting:
             self.flush()
-        self.write(b'GETMEAS\r')
+        self.write(b'GETMEAS\r') #add \n here???
         # wait for echo
         time.sleep(0.2)
         try:
@@ -53,8 +58,8 @@ class meter(serial.Serial):
                 print(line)
                 meas_list = line.split(',')
                 if meas_list:
-                    mV = float(meas_list[5])
-                    T = float(meas_list[7])
+                    mV = float(meas_list[10])
+                    T = float(meas_list[12])
                 return (mV,T)
         except:
              print('no response')
@@ -62,10 +67,14 @@ class meter(serial.Serial):
         
 class pump(serial.Serial):
     """
-    Serial device object for milligat LF pump with microlynx controller
+    original controller uLynx
+    Serial device object for milligat LF pump with MFORCE controller
     Be sure pump is powered and serial cable connected
-    """  
-    TERMINATOR = '\r\n'
+    since this is a 422 device, it requires an address which precedes each comman
+    default is A
+    """
+    #TERMINATOR = '\r\n'
+    TERMINATOR = '\r\n' ##########################################################################
     # redefine readline to work for \r line termination
     def readline(self,eol=TERMINATOR.encode('utf-8')):
         leneol = len(eol)
@@ -86,9 +95,12 @@ class pump(serial.Serial):
     
     def getVar(self,var,eol=TERMINATOR):
         self.reset_input_buffer()
-        bmsg = ('print ' + var.lower() + eol).encode('utf-8')
+        #bmsg = ('PR ' + var.lower() + eol).encode('utf-8')
+        bmsg = ('PR n ' + var.lower() + eol).encode('utf-8')
+        print(var.lower)
+        print(var.lower())
         self.write(bmsg)
-        time.sleep(0.2)
+        time.sleep(.5)
         if self.in_waiting:
             bline = self.readline()
             # if command is echoed, read next line
@@ -102,11 +114,11 @@ class pump(serial.Serial):
             
     def setPos(self,val,eol=TERMINATOR):
         valstr = str(val)
-        self.write(('pos =' + valstr + eol).encode('utf-8'))
+        self.write(('P =' + valstr + eol).encode('utf-8'))
         
     def getPos(self,eol=TERMINATOR):
         self.reset_input_buffer()
-        bmsg = ('print pos' + eol).encode('utf-8')
+        bmsg = ('PR P' + eol).encode('utf-8')
         self.write(bmsg)
         time.sleep(0.2)
         if self.in_waiting:
@@ -121,20 +133,36 @@ class pump(serial.Serial):
     
          
     def movr(self,uL,eol=TERMINATOR):
-        # dispense - relative pump movement 
-        self.write(('movr ' + uL + eol).encode('utf-8'))
+        # dispense - relative pump movement
+        # 1 ul = 23104 steps
+        steps = int(uL)*23104
+        print ('MR ' + str(steps))
+        self.write(('MR ' + str(steps) + eol).encode('utf-8'))
 
                
     def mova(self,uL,eol=TERMINATOR):
-        # dispense - move pump to absolute position 
-        self.write(('mova ' + uL + eol).encode('utf-8'))
+        # dispense - move pump to absolute position
+        steps = int(uL)*23104
+        print ('MA ' + str(steps))
+        self.write(('MA ' + str(steps) + eol).encode('utf-8'))
     
+    def setVM(self,uL,eol=TERMINATOR):
+        # dispense - set rate
+        steps = int(uL)*23104
+        print('VM ' + str(steps))
+        self.write(('VM ' + str(steps) + eol).encode('utf-8'))
+
     def wait_for_dispense(self,uL,eol=TERMINATOR):
+        #uLynx
+        #called from titration.py
         # maximum rate in uL sec-1
-        max_rate = self.getVar('VM')
+        #self.write(('MR ' + str(uL*23104) + eol).encode('utf-8'))
+        max_rate = self.getVar('VM') # steps per second
+        steps = int(uL)*23104
+        #max_rate = steps
         # wait for dispense to complete (add 0.2 secs for accel/decel)
-        wait_time = uL / float(max_rate) + 0.2
+        wait_time = steps / max_rate + 0.2
+        print('wait time ' + str(wait_time))
         return wait_time
         
         
-
